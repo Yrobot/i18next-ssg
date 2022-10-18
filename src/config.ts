c691c2a8a4b8239f0890eca9cb765044500e89b8
Yrobot <yrobot@qq.com>
2022-10-18 17:47:31 +0800

    update ts config

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,18 @@
 import path from "path";
-import { UserConfig } from "next-i18next";
 import { Locale } from "./types";
 
 const DEFAULT_CONFIG_PATH = "./next-i18next.config.js";
 
+export type I18nConfig = {
+  defaultLocale: Locale;
+  locales: Locale[];
+};
+
 const config: {
-  i18n: UserConfig["i18n"];
-} = require(DEFAULT_CONFIG_PATH);
+  i18n: I18nConfig;
+} = require(path.resolve(DEFAULT_CONFIG_PATH));
 
 export const i18n = config.i18n;
 
-export const locales = i18n.locales as Locale[];
-export const defaultLocale = i18n.defaultLocale as Locale;
+export const locales = i18n.locales;
+export const defaultLocale = i18n.defaultLocale;
