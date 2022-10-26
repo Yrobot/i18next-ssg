e5eba05e8b1d065068d08d3ff594a05cd679000d
Yrobot <yrobot@qq.com>
2022-10-26 18:32:41 +0800

    finish config logic

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,18 @@
 import { Locale } from "./types";
 
-const CONFIG_PATH = "next-i18next.config.js";
-
-export const get = () => {
-  return require(CONFIG_PATH);
+export type I18nConfig = {
+  defaultLocale: Locale;
+  locales: Locale[];
 };
 
-// export type I18nConfig = {
-//   defaultLocale: Locale;
-//   locales: Locale[];
-// };
+const config: {
+  i18n: I18nConfig;
+} = require("next-i18next.config.js");
 
-// const config: {
-//   i18n: I18nConfig;
-// } = require(path.resolve(CONFIG_PATH));
+if (config?.i18n === undefined)
+  throw new Error(
+    `You have to define the 'i18n' in '${"next-i18next.config.js"}'`
+  );
 
-// if (config?.i18n === undefined)
-//   throw new Error(`You have to define the 'i18n' in '${CONFIG_PATH}'`);
-
-// export const locales = config.i18n.locales;
-// export const defaultLocale = config.i18n.defaultLocale;
+export const locales = config.i18n.locales;
+export const defaultLocale = config.i18n.defaultLocale;
