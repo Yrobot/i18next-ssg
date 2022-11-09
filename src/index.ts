e6df7ab8fd602de5bf24ac1a0df894f7a1586078
Yrobot <Yrobot@qq.com>
2022-11-09 18:05:10 +0800

    add ...paths logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * as config from "./config";
-// export * from "./getStatic";
-// export * from "./router";
-// export * from "./types";
-// export * from "./utilities";
+export * from "./router";
+export * from "./types";
+export * from "./utilities";
 export * from "next-i18next";
